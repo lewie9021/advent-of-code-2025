a3a4f04cfc7496a9867bf028c839183c08aab986
Lewis Barnes <lewis.barnes@sqa-consulting.com>
2025-02-01 18:20:56 +0000

    Solution for Day 3 - Part 2.

--- src/day-3/index.ts
+++ src/day-3/index.ts
@@ -5,21 +5,45 @@ import getDirname from "../helpers/getDirname.ts";
 const parseInput = () => {
   const contents = fs.readFileSync(path.join(getDirname(import.meta.url), "./input.txt"), "utf8");
 
-  return contents.match(/mul\(\d+,\d+\)/g);
+  return contents.match(/(mul\(\d+,\d+\))|(don't\(\))|(do\(\))/g);
 };
+
+const runMul = (instruction: string) => {
+  return instruction.match(/\d+/g)
+    .map((x) => parseFloat(x))
+    .reduce((res, x) => res * x);
+}
 
 const calculatePartOne = () => {
   return parseInput()
-    .map((instruction) => {
-      return instruction.match(/\d+/g)
-        .map((x) => parseFloat(x));
-    })
-    .map(([x, y]) => x * y)
-    .reduce((res, x) => res + x, 0);
+    .filter((instruction) => instruction.startsWith("mul("))
+    .map((instruction) => runMul(instruction))
+    .reduce((res, x) => res + x);
 };
 
 const calculatePartTwo = () => {
-  return null
+  let enabled = true;
+  let total = 0;
+
+  const instructions = parseInput();
+
+  for (let i = 0; i < instructions.length; i += 1) {
+    const instruction = instructions[i];
+
+    if (instruction.startsWith("do(")) {
+      enabled = true;
+    }
+
+    if (instruction.startsWith("don't(")) {
+      enabled = false;
+    }
+
+    if (instruction.startsWith("mul(") && enabled) {
+      total += runMul(instruction);
+    }
+  }
+
+  return total;
 };
 
 console.log("Part One:", calculatePartOne());
