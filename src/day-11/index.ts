d4c2124b9572e5551b11a755a5cc2b69677ddd77
Lewis Barnes <lewis.barnes@sqa-consulting.com>
2025-02-13 16:32:51 +0000

    Solution for Day 11 - Part 2.

--- src/day-11/index.ts
+++ src/day-11/index.ts
@@ -1,8 +1,7 @@
-import { Tuple } from "../helpers/tuple";
-
 import * as fs from "fs";
 import * as path from "path";
 import getDirname from "../helpers/getDirname.ts";
+
 
 const parseInput = () => {
   const contents = fs.readFileSync(path.join(getDirname(import.meta.url), "./input.txt"), "utf8");
@@ -46,6 +45,52 @@ const blink = (stones: Array<number>) => {
   }, []);
 };
 
+const cache: Record<string, number> = {};
+
+const getCacheKey = (stone: number, blinks: number) => [stone, blinks].join("|");
+
+const blinkV2 = (stones: Array<number>, blinks: number) => {
+  if (blinks < 1) {
+    return stones.length;
+  }
+
+  return stones.reduce((total, stone) => {
+    const cacheResult = cache[getCacheKey(stone, blinks)];
+
+    if (typeof cacheResult !== "undefined") {
+      return total + cacheResult;
+    }
+
+    if (stone === 0) {
+      const result = blinkV2([1], blinks - 1);
+      cache[getCacheKey(1, blinks - 1)] = result;
+
+      return total + result;
+    }
+
+    const digits = getDigits(stone);
+
+    if (digits.length % 2 === 0) {
+      const middle = digits.length / 2;
+      const leftDigits = parseInt(digits.slice(0, middle).join(""));
+      const rightDigits = parseInt(digits.slice(middle).join(""));
+      const leftResult = blinkV2([leftDigits], blinks - 1);
+      const rightResult = blinkV2([rightDigits], blinks - 1);
+      const result = leftResult + rightResult;
+
+      cache[getCacheKey(leftDigits, blinks - 1)] = leftResult;
+      cache[getCacheKey(rightDigits, blinks - 1)] = rightResult;
+
+      return total + result;
+    }
+
+    const result = blinkV2([stone * 2024], blinks - 1);
+    cache[getCacheKey(stone * 2024, blinks - 1)] = result;
+
+    return total + result;
+  }, 0);
+};
+
 const calculatePartOne = () => {
   let result = parseInput();
 
@@ -57,7 +102,9 @@ const calculatePartOne = () => {
 };
 
 const calculatePartTwo = () => {
-  return null;
+  const stones = parseInput();
+
+  return blinkV2(stones, 75);
 };
 
 console.log("Part One:", calculatePartOne());
